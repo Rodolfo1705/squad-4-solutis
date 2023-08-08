14c474b7839f5973cf3d50b9882a1e50392592f4
Rodolfo1705 <roabrahao5@gmail.com>
2023-08-08 10:51:53 -0300

    removing lista-3 dir

--- lista-3/src/Utils/Divisor.java
+++
@@ -1,7 +0,0 @@
-package Utils;
-
-public class Divisor {
-    public static void printExercise(int exerciseNumber){
-        System.out.println("====== Ex." + exerciseNumber + " ======\n");
-    }
-}
