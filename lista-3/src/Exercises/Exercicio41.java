963965dfe73669b0cf773c43462d7c95f3d738db
Rodolfo1705 <roabrahao5@gmail.com>
2023-08-08 10:01:04 -0300

    starting exercise 41

--- lista-3/src/Exercises/Exercicio41.java
+++ lista-3/src/Exercises/Exercicio41.java
@@ -1,11 +1,22 @@
 package Exercises;
 
 import Utils.Divisor;
+import Utils.InputHandler;
+
+import java.util.Random;
 
 public class Exercicio41 {
     public static void run(){
         Divisor.printExercise(41);
 
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(100);
+
+        int attemptNumber = InputHandler.getIntInput("Digite um valor ");
+
+    }
+
+    private static void game(int randomNumber, int attemptNumber){
 
     }
 }
