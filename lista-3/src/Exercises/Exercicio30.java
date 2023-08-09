499a9029a6692003fa7764c6f9c5856ce9ddf346
Rodolfo1705 <roabrahao5@gmail.com>
2023-08-08 23:12:08 -0300

    feat: finishing exercise 30

--- lista-3/src/Exercises/Exercicio30.java
+++ lista-3/src/Exercises/Exercicio30.java
@@ -9,36 +9,18 @@ public class Exercicio30 {
 
 		int firstRectangleX1 = InputHandler.getIntInput("Digite o valor X de um vértice: ");
 		int firstRectangleY1 = InputHandler.getIntInput("Digite o valor Y de um vértice: ");
-		int firstRectangleX2 = InputHandler.getIntInput("Digite o valor X de outro vértice: ");
-		int firstRectangleY2 = InputHandler.getIntInput("Digite o valor Y de outro vértice: ");
-		int firstRectangleX3 = InputHandler.getIntInput("Digite o valor X de mais um vértice: ");
-		int firstRectangleY3 = InputHandler.getIntInput("Digite o valor Y de mais um vértice: ");
-		int firstRectangleX4 = InputHandler.getIntInput("Digite o valor X do último vértice: ");
-		int firstRectangleY4 = InputHandler.getIntInput("Digite o valor Y do último vértice: ");
+		int firstRectangleX2 = InputHandler.getIntInput("Digite o valor X do outro vértice: ");
+		int firstRectangleY2 = InputHandler.getIntInput("Digite o valor Y do outro vértice: ");
 
 		int secondRectangleX1 = InputHandler.getIntInput("Digite o valor X de um vértice: ");
 		int secondRectangleY1 = InputHandler.getIntInput("Digite o valor Y de um vértice: ");
-		int secondRectangleX2 = InputHandler.getIntInput("Digite o valor X de outro vértice: ");
-		int secondRectangleY2 = InputHandler.getIntInput("Digite o valor Y de outro vértice: ");
-		int secondRectangleX3 = InputHandler.getIntInput("Digite o valor X de mais um vértice: ");
-		int secondRectangleY3 = InputHandler.getIntInput("Digite o valor Y de mais um vértice: ");
-		int secondRectangleX4 = InputHandler.getIntInput("Digite o valor X do último vértice: ");
-		int secondRectangleY4 = InputHandler.getIntInput("Digite o valor Y do último vértice: ");
+		int secondRectangleX2 = InputHandler.getIntInput("Digite o valor X do outro vértice: ");
+		int secondRectangleY2 = InputHandler.getIntInput("Digite o valor Y do outro vértice: ");
 
-        
+		if (firstRectangleX1 > secondRectangleX2 || firstRectangleX2 < secondRectangleX1 || firstRectangleY1 > secondRectangleY2 || firstRectangleY2 < secondRectangleY1) {
+			System.out.println("Os retângulos não se sobrepõem.");
+		} else {
+			System.out.println("Os retângulos se sobrepõem em algum lugar.");
+		}
 	}
-
-	private static boolean isRectanglePointsValid(int x1, int y1, int x2, int y2,int x3, int y3,int x4, int y4){
-		if ((x1 == x2 && y1 == y2) || (x1 == x3 && y1 == y3) || (x1 == x4 && y1 == y4))
-			return false;
-
-		if((x2 == x3 && y2 == y3) || (x2 == x4 && y2 == y4))
-			return false;
-
-		if(x3 == x4 && y3 ==y4)
-			return false;
-
-		return true;
-	}
-
 }
